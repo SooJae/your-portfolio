e42d0e38a89dced6b714ca5f7479f2e5f8f2d081
soojae <developersoojae@gmail.com>
2020-11-04 08:34:15 +0900

    Init project

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  compress: true,
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === 'production';
+    const plugins = [...config.plugins];
+    if (prod) {
+      // plugins.push(추가할 플러그인);
+    }
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval',
+      plugins,
+    };
+  },
+});
